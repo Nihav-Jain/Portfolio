816ac527a5816d6b555712ae468bdd041b0f97ec
Nihav Jain <nihav.jain13@gmail.com>
2015-08-09 07:09:01 -0700

    added static multi-level slide push menu (not working for smaller screens)

--- js/directives/menuDirective.js
+++ js/directives/menuDirective.js
@@ -10,60 +10,66 @@ portfolioApp.directive('menuDirective',['menuService', function(menuService) {
       templateUrl:'views/menu.html',
       controller: function($scope, $element, $http, $window, $anchorScroll) {
 
-        var w = angular.element($window);
-        w.bind('resize', function(){
-          $scope.$apply();
-        });
+        $scope.newMenu = false;
         
-        $scope.menus = [];
-        $scope.activeMenuIndex = 0;
-        $scope.menuStatus = [];
+        $scope.openMenu = function(){
+          $scope.newMenu = !$scope.newMenu;
+        };
 
-        // $http.get('data/menu.json').success(function(data){
-        menuService.getMenuData().success(function(data){  
-          // console.log(data);
-          $scope.menus = data.menus;
-          menuService.menus = data.menus;
-          menuService.selectedTile = menuService.menus[0].tiles[0];
-          var i;
-          for(i=0;i<$scope.menus.length;i++){
-            $scope.menuStatus.push(false);
-          }
-          $scope.menuStatus[$scope.activeMenuIndex] = true;
-        });
+        // var w = angular.element($window);
+        // w.bind('resize', function(){
+        //   $scope.$apply();
+        // });
         
-        $scope.menuTileClicked = function($index){
-          if($scope.menus[$scope.activeMenuIndex].tiles[$index].nextMenu){
-            $scope.activeMenuIndex = $scope.menus[$scope.activeMenuIndex].tiles[$index].nextMenu;
-            $scope.updateMenuStatus();
-          }
-          menuService.selectedTile = menuService.menus[$scope.activeMenuIndex].tiles[$index];
-          $anchorScroll();
-        };
+        // $scope.menus = [];
+        // $scope.activeMenuIndex = 0;
+        // $scope.menuStatus = [];
 
-        $scope.backClicked = function(){
-          $scope.activeMenuIndex = $scope.menus[$scope.activeMenuIndex].backMenuIndex;
-          $scope.updateMenuStatus();
-          menuService.selectedTile = menuService.menus[$scope.activeMenuIndex].tiles[0];
-          $anchorScroll();
-        };
+        // // $http.get('data/menu.json').success(function(data){
+        // menuService.getMenuData().success(function(data){  
+        //   // console.log(data);
+        //   $scope.menus = data.menus;
+        //   menuService.menus = data.menus;
+        //   menuService.selectedTile = menuService.menus[0].tiles[0];
+        //   var i;
+        //   for(i=0;i<$scope.menus.length;i++){
+        //     $scope.menuStatus.push(false);
+        //   }
+        //   $scope.menuStatus[$scope.activeMenuIndex] = true;
+        // });
+        
+        // $scope.menuTileClicked = function($index){
+        //   if($scope.menus[$scope.activeMenuIndex].tiles[$index].nextMenu){
+        //     $scope.activeMenuIndex = $scope.menus[$scope.activeMenuIndex].tiles[$index].nextMenu;
+        //     $scope.updateMenuStatus();
+        //   }
+        //   menuService.selectedTile = menuService.menus[$scope.activeMenuIndex].tiles[$index];
+        //   $anchorScroll();
+        // };
 
-        $scope.updateMenuStatus = function(){
-          var i;
-          for(i=0;i<$scope.menuStatus.length;i++){
-            $scope.menuStatus[i] = false;
-          }
-          $scope.menuStatus[$scope.activeMenuIndex] = true;
-        };
+        // $scope.backClicked = function(){
+        //   $scope.activeMenuIndex = $scope.menus[$scope.activeMenuIndex].backMenuIndex;
+        //   $scope.updateMenuStatus();
+        //   menuService.selectedTile = menuService.menus[$scope.activeMenuIndex].tiles[0];
+        //   $anchorScroll();
+        // };
 
-        $scope.getMenuStatus = function($index){
-          // return $scope.menuStatus[$index];
-          return $scope.menuStatus[$index] || ($index <= $scope.activeMenuIndex);
-        };
+        // $scope.updateMenuStatus = function(){
+        //   var i;
+        //   for(i=0;i<$scope.menuStatus.length;i++){
+        //     $scope.menuStatus[i] = false;
+        //   }
+        //   $scope.menuStatus[$scope.activeMenuIndex] = true;
+        // };
 
-        $scope.navViewCondition = function(){
-          return !($window.innerWidth < 1000);
-        };
+        // $scope.getMenuStatus = function($index){
+        //   // return $scope.menuStatus[$index];
+        //   return $scope.menuStatus[$index] || ($index <= $scope.activeMenuIndex);
+        // };
+
+        // $scope.navViewCondition = function(){
+        //   return !($window.innerWidth < 1000);
+        // };
       }
   };
 }]);
