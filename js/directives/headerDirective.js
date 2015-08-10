b26e0a100fcc30e01eeb85f6c1e0da5e49ae3496
Nihav Jain <nihav.jain13@gmail.com>
2015-08-10 12:26:46 -0700

    fixed header css

--- js/directives/headerDirective.js
+++ js/directives/headerDirective.js
@@ -8,18 +8,18 @@ portfolioApp.directive('headerDirective',['menuService', function(menuService) {
       replace: 'true',
       templateUrl:'views/header.html',
       require: '^menuService',
-      controller: function($scope, $element, $state) {
-        // console.log("headerdirective");
+      controller: function($scope, $element) {
+        console.log("headerdirective");
         $scope.backgroundClass = "about-header";
-        $scope.$watch(function(){
-          return menuService.selectedTile;
-        }, function(value){
-          if(value){
-            var newClass = value.colorClass;
-            newClass = newClass.substring(0, newClass.indexOf('-')+1);
-            $scope.backgroundClass = newClass + "header";
-          }
-        });
+        // $scope.$watch(function(){
+        //   return menuService.selectedTile;
+        // }, function(value){
+        //   if(value){
+        //     var newClass = value.colorClass;
+        //     newClass = newClass.substring(0, newClass.indexOf('-')+1);
+        //     $scope.backgroundClass = newClass + "header";
+        //   }
+        // });
       }
   };
 }]);
