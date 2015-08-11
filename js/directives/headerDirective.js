a9362d4031d1167d3745dff720fa8f68836a4989
Nihav Jain <nihav.jain13@gmail.com>
2015-08-11 13:54:30 -0700

    responsive multi-level slide push menu

--- js/directives/headerDirective.js
+++ js/directives/headerDirective.js
@@ -7,6 +7,15 @@ portfolioApp.directive('headerDirective',['menuService', function(menuService) {
       restrict: 'AE',
       replace: 'true',
       templateUrl:'views/header.html',
-      require: 'menuDirective'
+      require: 'menuDirective',
+      controller: function($scope, $element){
+      	
+		angular.element(".toggle-btn").data("toggle-btn", true);
+      	var toggleBtnElement = angular.element(".toggle-btn");
+        toggleBtnElement.bind('click', function(){
+            $scope.toggleBtnClicked();
+        });
+
+      }
   };
 }]);
